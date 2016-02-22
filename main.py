2a7da1fdfd3fe27a4f6784ce3dc3b25af3dca2a3
Daniel Ayoub <daniel.ayoub@rutgers.edu>
2016-02-22 18:05:39 +0000

    verified threading

--- main.py
+++ main.py
@@ -23,18 +23,18 @@ class fifo(object):
     def read(self):
         return self.buf.pop(0)
 
-f = fifo()
-mav = mavlink.MAVLink(f)
-
 class rfLink (threading.Thread):
 	src = None
 	dest = None
 	name = None
+	mav = None
 	def __init__(self, src, dest, name, **kwds):
 		threading.Thread.__init__(self)
 		self.src = src
 		self.dest = dest
 		self.name = name
+		self.f = fifo()
+		self.mav = mavlink.MAVLink(self.f)
 		try:
 			if(kwds['daemon']): 
 				self.daemon = True
@@ -42,6 +42,7 @@ class rfLink (threading.Thread):
 				self.daemon = False
 		except KeyError:
 			pass
+	
 	def run(self):
 		self.src.open()
 		self.dest.open()
@@ -52,13 +53,32 @@ class rfLink (threading.Thread):
 	def handover(self):
 		print 'Started ' + self.name
 		while True:
-			if (self.src.inWaiting()>0):
-				g = mav.parse_buffer(self.src.read())
-				if (g!=None):
-					self.dest.write(str(bytearray(f)))
+			try:
+				g = self.mav.parse_buffer(self.src.read())
+			except mavlink.MAVError:
+				pass
+			if (g!=None):
+				#self.dest.write(str(bytearray(self.f)))
+				print(g)
+                                print(g[0].type)
+                                print(g[0].autopilot)
+                                print(g[0].base_mode)
+                                print(g[0].custom_mode)
+                                print(g[0].system_status)
+                                print(g[0].mavlink_version)
+				self.src.write(str(bytearray(self.f.buf)))
 				
 rTa = rfLink(radio, apm, 'Radio to APM')
 aTr = rfLink(apm, radio, 'APM to Radio', daemon=True)
 rTa.start()
 aTr.start()
 
+#f = fifo()
+#mav = mavlink.MAVLink(f)
+
+#radio.open()
+#radio.flushInput() 
+#while True:
+#	g = mav.parse_buffer(radio.read(1))
+#	if (g != None):
+#		print(g)
